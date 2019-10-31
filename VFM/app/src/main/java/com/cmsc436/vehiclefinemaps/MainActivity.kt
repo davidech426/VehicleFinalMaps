0b67fdd3dedeed4fd1bc3328e6e1f141037e8af9
dechever <davidecheverria97@gmail.com>
2019-10-31 19:50:32 -0400

    auth firebase done

---
+++ VFM/app/src/main/java/com/cmsc436/vehiclefinemaps/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.cmsc436.vehiclefinemaps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+
+
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.auth.FirebaseAuth
+
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+
+
+class MainActivity : AppCompatActivity() {
+
+    //Firebase references
+    private var mDatabaseReference: DatabaseReference? = null
+    private var mDatabase: FirebaseDatabase? = null
+    private var mAuth: FirebaseAuth? = null
+    //UI elements
+    private var tvFirstName: TextView? = null
+    private var tvLastName: TextView? = null
+    private var tvEmail: TextView? = null
+    private var tvEmailVerifiied: TextView? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initialise()
+    }
+
+    private fun initialise() {
+        mDatabase = FirebaseDatabase.getInstance()
+        mDatabaseReference = mDatabase!!.reference.child("Users")
+        mAuth = FirebaseAuth.getInstance()
+        tvFirstName = findViewById<View>(R.id.tv_first_name) as TextView
+        tvLastName = findViewById<View>(R.id.tv_last_name) as TextView
+        tvEmail = findViewById<View>(R.id.tv_email) as TextView
+        tvEmailVerifiied = findViewById<View>(R.id.tv_email_verifiied) as TextView
+
+
+    }
+    override fun onStart() {
+        super.onStart()
+        val mUser = mAuth!!.currentUser
+        val mUserReference = mDatabaseReference!!.child(mUser!!.uid)
+        tvEmail!!.text = mUser.email
+        tvEmailVerifiied!!.text = mUser.isEmailVerified.toString()
+        mUserReference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                tvFirstName!!.text = snapshot.child("firstName").value as String
+                tvLastName!!.text = snapshot.child("lastName").value as String
+            }
+            override fun onCancelled(databaseError: DatabaseError) {}
+        })
+    }
+}
