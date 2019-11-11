482d610c4b043967237c36fc33392fba09db9614
Koonj Patel <Koonj@wireless-10-105-36-11.umd.edu>
2019-11-10 20:03:30 -0500

    added hamburger-like options menu

--- VFM/app/src/main/java/com/cmsc436/vehiclefinemaps/ChoosingOption.kt
+++ VFM/app/src/main/java/com/cmsc436/vehiclefinemaps/ChoosingOption.kt
@@ -1,19 +1,29 @@
 package com.cmsc436.vehiclefinemaps
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.cmsc436.vehiclefinemaps.R.id.btn_letsDrive
 import kotlinx.android.synthetic.main.choosing_option.*
+//import androidx.navigation.ui.AppBarConfiguration
 
 class ChoosingOption : AppCompatActivity() {
 
-    val LetsDriveBtn = findViewById<Button>(R.id.btn_letsDrive) as Button
-    val ShowPreviousDrivesBtn = findViewById<Button>(R.id.btn_showprev) as Button
+    lateinit var LetsDriveBtn: Button
+    lateinit var ShowPreviousDrivesBtn: Button
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.choosing_option)
+
+        LetsDriveBtn = findViewById<Button>(R.id.btn_letsDrive) as Button
+        ShowPreviousDrivesBtn = findViewById<Button>(R.id.btn_showprev) as Button
+
 
         LetsDriveBtn.setOnClickListener {
             //Display current drive
@@ -25,6 +35,28 @@ class ChoosingOption : AppCompatActivity() {
 
         }
 
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.profile -> {
+
+                //TODO: THE ACTIVITY SHOULD CALL SOMETHING THAT DISPLAYS THE USER'S INFORMATION
+                val i = Intent(this@ChoosingOption, LoginActivity::class.java)
+                startActivity(i)
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
 
