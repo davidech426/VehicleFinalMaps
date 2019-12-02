35c7b8d010905de77fd1af84511392ed2fd0b210
Nicholas <Nick@Nicholass-MacBook-Pro-3.local>
2019-12-02 07:00:43 -0500

    Added location and speed gathering. Risk analysis done.

--- VFM/app/src/main/java/com/cmsc436/vehiclefinemaps/LetsDrive.kt
+++ VFM/app/src/main/java/com/cmsc436/vehiclefinemaps/LetsDrive.kt
@@ -1,18 +1,27 @@
 package com.cmsc436.vehiclefinemaps
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.Location
 import android.location.LocationManager
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.StrictMode
+import android.provider.Settings
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.core.view.isVisible
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.*
 import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.TimeUnit
@@ -26,7 +35,7 @@ import com.google.firebase.database.DataSnapshot
 
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
-
+import java.net.URL
 
 
 class LetsDrive : AppCompatActivity(){
@@ -46,6 +55,27 @@ class LetsDrive : AppCompatActivity(){
     private var mAuth: FirebaseAuth? = null
 
 
+    val mainHandler = Handler(Looper.getMainLooper())
+    val handler = Handler()
+    private lateinit var mRunnable:Runnable
+
+    val app_id = "iN2zzYvXRAmxbvmz5vMw"
+    val app_code = "Djl7Hs_LIhwN2tmPfQGiNA"
+
+    var lat1: Double? = null
+    var lat2: Double? = null
+
+    var lng1: Double? = null
+    var lng2: Double? = null
+
+    var limit = 0.0f
+    var speed = 56.0f
+
+    var avSpeed = 0.0f
+    var avRisk = 0.0f
+    var counter = 0
+
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -57,6 +87,9 @@ class LetsDrive : AppCompatActivity(){
         findViewById<Button>(R.id.end_button).visibility = View.GONE
         findViewById<Button>(R.id.start_btn).visibility = View.VISIBLE
 
+        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
+
         mDatabase = FirebaseDatabase.getInstance()
         mDatabaseReference = mDatabase!!.reference!!.child("Previous")
         mAuth = FirebaseAuth.getInstance()
@@ -64,7 +97,8 @@ class LetsDrive : AppCompatActivity(){
         buttonCount = 0
 
         val text = findViewById<TextView>(R.id.percent)
-        text.setText(Random.nextInt(100).toString() + "%")
+        text.setText("0%")
+
 
 
         if(checkPermissions() == false) {
@@ -88,6 +122,18 @@ class LetsDrive : AppCompatActivity(){
             } else {
                 findViewById<Button>(R.id.end_button).visibility = View.VISIBLE
                 findViewById<Button>(R.id.start_btn).visibility = View.GONE
+                mainHandler.post(object : Runnable {
+                    override fun run() {
+                        getLastLocation()
+                        mainHandler.postDelayed(this, 5000)
+                    }
+                })
+                handler.post(object : Runnable {
+                    override fun run() {
+                        getSpeedLimit()
+                        mainHandler.postDelayed(this, 30000)
+                    }
+                })
             }
             //set the start time of the duration
             startTime=System.currentTimeMillis()
@@ -100,8 +146,8 @@ class LetsDrive : AppCompatActivity(){
             if (buttonCount % 2 == 0) {
                 findViewById<Button>(R.id.end_button).visibility = View.GONE
                 findViewById<Button>(R.id.start_btn).visibility = View.VISIBLE
+                mainHandler.removeCallbacksAndMessages(null)
             } else {
-
                 findViewById<Button>(R.id.end_button).visibility = View.VISIBLE
                 findViewById<Button>(R.id.start_btn).visibility = View.GONE
             }
@@ -146,17 +192,115 @@ class LetsDrive : AppCompatActivity(){
             currentUser.child(driveId).child("date").setValue(dateOfDrive)
             currentUser.child(driveId).child("time").setValue(timeOfDrive)
             currentUser.child(driveId).child("duration").setValue(durationFormat)
+            //currentUser.child(driveId).child("speed").setValue(avSpeed)
+            //currentUser.child(driveId).child("likelihood").setValue(avRisk)
 
 
         }
-
-
-
-
-
     }
 
+    @SuppressLint("MissingPermission")
+    private fun getLastLocation() {
+        if (checkPermissions()) {
+            if (isLocationEnabled()) {
 
+                mFusedLocationClient.lastLocation.addOnCompleteListener(this) { task ->
+                    var location: Location? = task.result
+                    if (location == null) {
+                        requestNewLocationData()
+                    } else {
+                        requestNewLocationData()
+                        Log.d("LOCATION", "Latitude: " + location.latitude.toString())
+                        Log.d("LOCATION", "Longitude: " + location.longitude.toString())
+
+                        if (lat1 == null) {
+                            lat1 = location.latitude
+                            lng1 = location.longitude
+                        } else {
+                            lat2 = location.latitude
+                            lng2 = location.longitude
+                        }
+
+                        val text = findViewById<TextView>(R.id.percent)
+
+                        //speed is not set within emulator
+                        //val speed = location.speed as Float
+                        var risk = 0.0f
+                        if (speed > limit) {
+                            val diff = speed - limit
+                            if (limit < 55) {
+                                if (diff <= 16) {
+                                    risk = ((diff / 32) * 100)
+                                } else {
+                                    risk = ((diff / 55) + .5f) * 100f
+                                    if (risk > 100f) {
+                                        risk = 100f
+                                    }
+                                }
+                            }
+                            else {
+                                if (diff <= 11) {
+                                    risk = (diff / 22) * 100f
+                                } else {
+                                    risk = ((diff / 40f) + .5f) * 100f
+                                    if (risk > 100f) {
+                                        risk = 100f
+                                    }
+                                }
+                            }
+                        }
+                        text.setText(risk.toString() + "%")
+                        counter += 1
+                        avRisk += risk
+                        avSpeed += speed
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Turn on location", Toast.LENGTH_LONG).show()
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+            }
+        } else {
+            requestPermissions()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun requestNewLocationData() {
+        var mLocationRequest = LocationRequest()
+        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        mLocationRequest.interval = 0
+        mLocationRequest.fastestInterval = 0
+        mLocationRequest.numUpdates = 1
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        mFusedLocationClient!!.requestLocationUpdates(
+            mLocationRequest, mLocationCallback,
+            Looper.myLooper()
+        )
+    }
+
+    private val mLocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            var mLastLocation: Location = locationResult.lastLocation
+            Log.d("LOCATION", mLastLocation.latitude.toString())
+            Log.d("LOCATION", mLastLocation.longitude.toString())
+            if (lat1 == null) {
+                lat1 = mLastLocation.latitude
+                lng1 = mLastLocation.longitude
+            } else {
+                if (lat2 == null) {
+                    lat2 = mLastLocation.latitude
+                    lng2 = mLastLocation.longitude
+                } else {
+                    lat1 = lat2
+                    lng1 = lng2
+                    lat2 = mLastLocation.latitude
+                    lng2 = mLastLocation.longitude
+                }
+            }
+        }
+    }
 
     private fun checkPermissions(): Boolean {
 
@@ -184,5 +328,33 @@ class LetsDrive : AppCompatActivity(){
         )
     }
 
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        if (requestCode == PERMISSION_ID) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                getLastLocation()
+            }
+        }
+    }
+
+    private fun getSpeedLimit() {
+        if (lat1 != null && lat2 != null) {
+            val request =
+                "https://route.cit.api.here.com/routing/7.2/calculateroute.json?jsonAttributes=1&waypoint0=$lat1,$lng1&waypoint1=$lat2,$lng2&departure=2019-01-18T10:33:00&routeattributes=sh,lg&legattributes=li&linkattributes=nl,fc&mode=fastest;car;traffic:enabled&app_code=$app_code&app_id=$app_id"
+            Log.d("response", request)
+            val ret = URL(request).readText()
+            Log.d("response", ret)
+            var retParsed = ret.substringAfter(""""speedLimit":""")
+            if (retParsed != null) {
+                retParsed = retParsed.substringBefore(".")
+            }
+            if (retParsed != null) {
+                limit = retParsed.toFloat()
+                limit *= 2.237f
+            }
+
+        }
+        lat1 = lat2
+        lng1 = lng2
+    }
 
 }
